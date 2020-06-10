1371c3d43efacfa91c1e84018bab5039e5521a98
Rich Song <richard.song@hbc.com>
2020-06-10 19:09:36 -0400

    Updates

---
+++ precache-manifest.b29ee176a7adccad069ba36cacfe26ef.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a739ead020fcf93c3a5bbc00be62a38",
+    "url": "/MisfitsMarket/index.html"
+  },
+  {
+    "revision": "d08eefd334b963e78422",
+    "url": "/MisfitsMarket/static/js/2.f4d50b59.chunk.js"
+  },
+  {
+    "revision": "4fce2f134288b7e76273",
+    "url": "/MisfitsMarket/static/js/main.eb9149a7.chunk.js"
+  },
+  {
+    "revision": "785662715ad0dbdbcc10",
+    "url": "/MisfitsMarket/static/js/runtime~main.5c219a04.js"
+  },
+  {
+    "revision": "e2c6c8c5b9ab33b0897c7a6a2b7c9421",
+    "url": "/MisfitsMarket/static/media/logo.e2c6c8c5.svg"
+  }
+]);

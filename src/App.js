8e860120d0047d6762a8b47361aca79d4d3e2a84
Rich Song <richard.song@hbc.com>
2020-06-11 16:13:28 -0400

    Fixed spacing issue

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ const GlobalStyle = createGlobalStyle`
     transform: rotatez(45deg);
     margin-top: -21%;
   }
-  
+
   li::before, li::after {
     display: block;
     position: absolute;
@@ -32,22 +32,23 @@ const GlobalStyle = createGlobalStyle`
     height: 100%;
     content: '';
   }
-  
+
   li::before {
     z-index: -10;
     clip-path: polygon(0 0, 100% 0, 100% 20%, 20% 20%, 20% 100%, 0 100%);
     -webkit-clip-path: polygon(0 0, 100% 0, 100% 20%, 20% 20%, 20% 100%, 0 100%);
   }
+
   li::after {
     z-index: 10;
     clip-path: polygon(80% 20%, 100% 0, 100% 100%, 0% 100%, 20% 80%, 80% 80%);
     -webkit-clip-path: polygon(80% 20%, 100% 0, 100% 100%, 0% 100%, 20% 80%, 80% 80%);
   }
-  
+
   li:nth-child(2n){
     grid-column-start:2;
   }
-  
+
   li{
     background-color: #EEBC1F;
   }
@@ -59,7 +60,7 @@ const GlobalStyle = createGlobalStyle`
     background-color: #068D7E;
     background: conic-gradient(#EEBC1F 75%, #068D7E 0) 0 0 /180% 180%;
   }
-  
+
   li:nth-child(2n){
     background-color: #FF5291;
   }
@@ -71,7 +72,7 @@ const GlobalStyle = createGlobalStyle`
     background-color: #4062BB;
     background: conic-gradient(#FF5291 75%, #4062BB 0) 0 0 /180% 180%;
   }
-  
+
   li:nth-child(5n){
     background-color: #068D7E;
   }
@@ -83,7 +84,7 @@ const GlobalStyle = createGlobalStyle`
     background-color: #FF5291;
     background: conic-gradient(#068D7E 75%, #FF5291 0) 0 0 /180% 180%;
   }
-  
+
   li:nth-child(7n),li:nth-child(7n-4){
     background-color: #4062BB;
   }
@@ -95,7 +96,7 @@ const GlobalStyle = createGlobalStyle`
     background-color: #F8FFE5;
     background: conic-gradient(#4062BB 75%, #F8FFE5 0) 0 0 /180% 180%;
   }
-  
+
   li:nth-child(9n),li:nth-child(9n-5){
     background-color: #F8FFE5;
   }
@@ -107,27 +108,28 @@ const GlobalStyle = createGlobalStyle`
     background-color: #068D7E;
     background: conic-gradient(#F8FFE5 75%, #068D7E 0) 0 0 /180% 180%;
   }
-  
+
   li img {
     position: absolute;
-    width: 60%;
+    width: 50%;
     left: 50%;
     top: 50%;
     transform: translateX(-50%) translateY(-50%) rotatez(-45deg);
-    padding-bottom: 15%;
     box-shadow: 5px -5px 10px rgba(0, 0, 0, 0.3);
     transition-property: transform;
     transition-duration: .3s;
   }
-  
-  li img:hover {
+
+  img:hover {
     transform: translateX(-60%) translateY(-60%) rotatez(-25deg);
   }
+
   @media (min-width:450px){
     ul{
       margin: 150px 40px;
     }
   }
+
   @media (min-width:600px){
     :root {
       --columns: 5;
@@ -139,6 +141,7 @@ const GlobalStyle = createGlobalStyle`
       grid-column-start:2;
     }
   }
+
   @media (min-width:900px){
     :root {
       --columns: 7;
@@ -150,6 +153,7 @@ const GlobalStyle = createGlobalStyle`
       grid-column-start:2;
     }
   }
+
   @media (min-width:1200px){
     :root {
       --columns: 9;
@@ -161,6 +165,7 @@ const GlobalStyle = createGlobalStyle`
       grid-column-start:2;
     }
   }
+
   @media (min-width:1500px){
     :root {
       --columns: 11;
@@ -172,6 +177,7 @@ const GlobalStyle = createGlobalStyle`
       grid-column-start:2;
     }
   }
+
   @media (min-width:1800px){
     :root {
       --columns: 13;
@@ -182,7 +188,7 @@ const GlobalStyle = createGlobalStyle`
     li:nth-child(12n-5){
       grid-column-start:2;
     }
-  
+
   }
   @media (min-width:2100px){
     :root {
@@ -194,7 +200,6 @@ const GlobalStyle = createGlobalStyle`
     li:nth-child(14n-6){
       grid-column-start:2;
     }
-  
   }
 
   body {

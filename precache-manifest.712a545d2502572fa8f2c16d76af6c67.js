24b0206bf09343a65b012b09f1212905a99d6a6d
Rich Song <richard.song@hbc.com>
2020-06-11 16:00:36 -0400

    Updates

--- precache-manifest.712a545d2502572fa8f2c16d76af6c67.js
+++ precache-manifest.712a545d2502572fa8f2c16d76af6c67.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "a5c79c5ed2811c28dc60d9bf676dd5da",
+    "revision": "ce8b3410814b539cb8f1d776c2b7c5fe",
     "url": "/MisfitsMarket/index.html"
   },
   {
@@ -8,8 +8,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/MisfitsMarket/static/js/2.f4d50b59.chunk.js"
   },
   {
-    "revision": "4c86c506de3c2440f517",
-    "url": "/MisfitsMarket/static/js/main.9b563343.chunk.js"
+    "revision": "99eac30d9e2579cb54b8",
+    "url": "/MisfitsMarket/static/js/main.0d036b8a.chunk.js"
   },
   {
     "revision": "785662715ad0dbdbcc10",

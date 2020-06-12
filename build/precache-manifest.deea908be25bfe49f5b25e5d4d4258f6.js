522f0fcb89a9e302b553beda443ccaafa453ceb1
Rich Song <richard.song@hbc.com>
2020-06-12 17:25:47 -0400

    Added sold out svg; Conditional rendering for null products

---
+++ build/precache-manifest.deea908be25bfe49f5b25e5d4d4258f6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3382e37772023362d341963591bbedfe",
+    "url": "/MisfitsMarket/index.html"
+  },
+  {
+    "revision": "ffa14fce121ad80fdb7e",
+    "url": "/MisfitsMarket/static/css/main.2cbe2f99.chunk.css"
+  },
+  {
+    "revision": "9033ba11a86166f0ceaf",
+    "url": "/MisfitsMarket/static/js/2.76d811bb.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/MisfitsMarket/static/js/2.76d811bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ffa14fce121ad80fdb7e",
+    "url": "/MisfitsMarket/static/js/main.16347623.chunk.js"
+  },
+  {
+    "revision": "086daf8db893d8d74954",
+    "url": "/MisfitsMarket/static/js/runtime-main.fe68f537.js"
+  },
+  {
+    "revision": "e2c6c8c5b9ab33b0897c7a6a2b7c9421",
+    "url": "/MisfitsMarket/static/media/logo.e2c6c8c5.svg"
+  },
+  {
+    "revision": "b1186c09a847c1090dd81a596ea48dfe",
+    "url": "/MisfitsMarket/static/media/sold-out.b1186c09.svg"
+  }
+]);
--- src/components/ProductGrid/ProductGrid.js
+++ src/components/ProductGrid/ProductGrid.js
@@ -1,60 +1,53 @@
 import React from 'react';
-import Product from '../Product/Product';
+import classnames from 'classnames'
 import './ProductGrid.css';
+import soldout from '../../assets/sold-out.svg'
 
 // TODO: need to reconsider design for product cards
 // should conditionally render; if products is null or sold out... render a sold out image
 // render a class for a product that is sold out
-//
 
-// https://previews.123rf.com/images/mearicon/mearicon1810/mearicon181000082/114177215-king-oyster-mushrooms-isolated-on-white.jpg
-const ProductGrid = ({ products }) => {
+const Soldout = () => {
   return (
-  <ul className="product-grid">
-    {
-      products.map((product) =>
-        <li className="product" key={product.id}>
-            <div className="product-detail">
-              <p>{product.price}</p>
-              <p>{product.product}</p>
-              <p>{product.msrp}</p>
-            </div>
-        </li>
-      )
-    }
-  </ul>
+    <span>
+      <img className="soldout" src={soldout} alt=""></img>
+      <p>Sorry! We are all sold out! Please come back soon!</p>
+    </span>
   )
 }
 
+const Product = ({ product }) => {
+  var cx = classnames({
+    "product-detail": true,
+    "product-available": !product.soldOut,
+    "product-soldout": product.soldOut
+  })
+  return (
+    <li className="product" key={product.id}>
+      <div className={cx}>
+        <p>Price: {product.price}</p>
+        <p>{product.product}</p>
+        <p>MSRP: {product.msrp}</p>
+      </div>
+    </li>
+    )
+}
+
+const ProductGrid = ({ products }) => {
+  products = null;
+  if (!products) {
+    return <Soldout />
+  } else {
+    return (
+    <ul className="product-grid">
+      {
+        products.map((product) =>
+          <Product product={product} />
+        )
+      }
+    </ul>
+    )
+  }
+}
+
 export default ProductGrid;
-
-// return (
-//   <ul>
-//     <li><img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/881020/book15.jpg" alt="" /></li>
-//     <li><img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/881020/book16.jpg" alt="" /></li>
-//     <li><img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/881020/book17.jpg" alt="" /></li>
-//     <li><img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/881020/book18.jpg" alt="" /></li>
-//     <li><img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/881020/book19.jpg" alt="" /></li>
-//     <li><img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/881020/book20.jpg" alt="" /></li>
-//     <li><img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/881020/book21.jpg" alt="" /></li>
-//     <li><img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/881020/book22.jpg" alt="" /></li>
-//     <li><img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/881020/book23.jpg" alt="" /></li>
-//     <li><img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/881020/book24.jpg" alt="" /></li>
-//     <li><img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/881020/book25.jpg" alt="" /></li>
-//     <li><img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/881020/book26.jpg" alt="" /></li>
-//   </ul>
-// )
-
-/* 
-king oyster mushroom
-canola oil 
-butternut squash
-green bell peppers
-pomelo
-frontier rosemary grill seasoning
-beets
-organic maitake mushrooms
-pineapple
-simple organic guacamole mix sauce
-pears
-*/

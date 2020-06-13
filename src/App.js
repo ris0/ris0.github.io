a1f1a550f29778f84780d41abb9490059b644a73
Rich Song <richard.song@hbc.com>
2020-06-13 18:00:55 -0400

    imrpoved success msg

--- src/App.js
+++ src/App.js
@@ -2,30 +2,40 @@ import React from 'react';
 import Logo from './components/Logo/Logo';
 import ProductGrid from './components/ProductGrid/ProductGrid';
 import SoldOut from './components/SoldOut/SoldOut';
+import SuccessMessage from './components/SuccessMessage/SuccessMessage';
 import './App.css';
 
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      products: []
+      products: [],
+      orderSuccessful: false
     };
   }
 
-  componentWillMount() {
+  componentDidMount() {
     fetch('https://applicant-dev.misfitsmarket.com/api/test/v1')
       .then(response => response.json())
-      .then(data => {
-        this.setState({products:data.data.items})
-      });
+      .then(data => { this.setState({products:data.data.items}) });
+  }
+
+  triggerSuccessMessage() {
+    this.setState({ orderSuccessful: true });
+    // setTimeout(() => this.setState({ orderSuccessful: false }), 1500);
   }
 
   render() {
     const products = this.state.products;
-    const content = !products || !products.length ? <SoldOut /> : <ProductGrid products={products}/>;
+    const content = !products || !products.length ? <SoldOut /> : <ProductGrid products={products} handleOrder={() => this.triggerSuccessMessage()}/>;
+    const successMsg = this.state.orderSuccessful ? <SuccessMessage /> : "";
+
     return (
       <div className="App">
-        <Logo />
+        <div className="header">
+          <Logo />
+          {successMsg}
+        </div>
         {content}
       </div>
     )
--- src/components/ProductGrid/ProductGrid.js
+++ src/components/ProductGrid/ProductGrid.js
@@ -1,55 +1,19 @@
 import React from 'react';
 import Product from '../Product/Product';
-import SuccessMessage from '../SuccessMessage/SuccessMessage';
-import SoldOut from '../SoldOut/SoldOut';
 import './ProductGrid.css';
 
-// TODO: How to fix the sold out flash? loading screen hack?
-
-class ProductGrid extends React.Component {
-  constructor(props) {
-    super(props);
-    this.triggerSuccessMessage = this.triggerSuccessMessage.bind(this);
-    this.state = { orderSuccessful: false }
-  }
-
-  triggerSuccessMessage() {
-    this.setState({orderSuccessful: true})
-    // setTimeout(() => this.setState({orderSuccessful: false}), 1000)
-  }
-
-  render() {
-    const successMsg = this.state.orderSuccessful ? <SuccessMessage /> : "";
-    return (
-      <div className="container">
-        <ul className="product-grid">
-          {
-            this.props.products.map((product) =>
-            <Product product={product} key={product.id} handleOrder={this.triggerSuccessMessage}/>
-            )
-          }
-          {successMsg}
-        </ul>
-      </div>
-    )
-  }
+const ProductGrid = ({ handleOrder, products }) => {
+  return (
+    <div className="container">
+      <ul className="product-grid">
+        {
+          products.map((product) =>
+            <Product product={product} key={product.id} handleOrder={handleOrder}/>
+          )
+        }
+      </ul>
+    </div>
+  )
 }
-
-
-// const ProductGrid = ({ products }) => {
-//   if (!products || !products.length) {
-//     return <Soldout />
-//   } else {
-//     return (
-//       <ul className="product-grid">
-//       {
-//         products.map((product) =>
-//           <Product product={product} key={product.id} />
-//         )
-//       }
-//     </ul>
-//     )
-//   }
-// }
 
 export default ProductGrid;
